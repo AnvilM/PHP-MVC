b9c9167cfcb607836b87c425517442eede57725f
AnvilM <211anvil112@gmail.com>
2023-12-11 19:35:08 +0300

    🔥 delete set method from Env class

--- src/lib/Env.php
+++ src/lib/Env.php
@@ -2,19 +2,13 @@
 
 namespace src\lib;
 
-class Env{
+class Env
+{
 
-    public static function get($key){
-        $env = file_get_contents($GLOBALS['ROOT'].'/src/env.json');
+    public static function get($key)
+    {
+        $env = file_get_contents($GLOBALS['ROOT'] . '/src/env.json');
         $env = json_decode($env, true);
-        return key_exists($key, $env) ? $env[$key] : 'key '.$key.' doesn\'t exist';
+        return key_exists($key, $env) ? $env[$key] : 'key ' . $key . ' doesn\'t exist';
     }
-
-    public static function set($key, $value){
-        $env = file_get_contents($GLOBALS['ROOT'].'/src/env.json');
-        $env = json_decode($env, true);
-        key_exists($key, $env) ? $env[$key] = $value : 'key '.$key.' doesn\'t exist';
-        file_put_contents($GLOBALS['ROOT'].'/src/env.json', json_encode($env));
-    }
-    
 }
