a575a647a2ffa21af7edd03f36081fbc2df442ef
AnvilM <211anvil112@gmail.com>
2023-12-24 18:50:39 +0300

    ✨ Added helpers support for nailer

--- console/Kernel.php
+++ console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace Console;
 
+use Console\Config\Config;
 use Console\Nailer\Nailer;
 
 class Kernel
@@ -9,6 +10,17 @@ class Kernel
     //Start nailer
     public function __construct($Arguments)
     {
+        $this->LoadHelpers();
+
         $Nailer = new Nailer($Arguments);
+    }
+
+    //Loads Helpers
+    private function LoadHelpers()
+    {
+        foreach (Config::get('helpers') as $Helper)
+        {
+            require_once ROOT . '/console/helpers/' . $Helper . '.php';
+        }
     }
 }
